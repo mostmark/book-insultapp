959a49ed1d4e3d26528ab2c0ce1b8c2d7bca08c3
mostmark <mostmark@example.com>
2017-05-16 22:05:09 +0100

    Did a simple change

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class InsultGenerator {
 	public String generateInsult() {
-		String words[][] = {{"Artless", "Bawdy", "Beslubbering"}, {"Base-court", "Bat-fowling", "Beef-witted"}, {"Apple-john", "Baggage", "Barnacle"}};
+		String words[][] = {{"Artlesssss", "Bawdy", "Beslubbering"}, {"Base-court", "Bat-fowling", "Beef-witted"}, {"Apple-john", "Baggage", "Barnacle"}};
 		String vowels = "AEIOU";
 		String article = "an";
 		String firstAdjective = words[0][new Random().nextInt(words[0].length)];
